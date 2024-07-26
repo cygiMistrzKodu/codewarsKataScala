f972392fbb46fba436311f81db7f1c16d9c6c84c
Jacek programmer <jacek301@gmail.com>
2024-07-26 14:21:40 +0200

    Duck Duck Goose kata solution

--- src/main/scala/RemoveFirstAndLastCharacters.scala
+++ src/main/scala/RemoveFirstAndLastCharacters.scala
@@ -1,5 +1,20 @@
-class  Player (name : String)
+case class Player(name: String)
 
 def duckDuckGoose(players: Seq[Player], goose: Int): String = {
-  ""
+
+  var chosen = ""
+  val allPlayers =  players.length
+  if (goose < allPlayers) {
+    chosen = players.apply(goose - 1).name
+  }
+
+  if (goose > allPlayers && goose % allPlayers != 0) {
+    chosen = players.apply(goose % allPlayers - 1).name
+  }
+
+  if (goose % allPlayers == 0) {
+    chosen = players.last.name
+  }
+
+  chosen
 }
